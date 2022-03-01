4c9756c593384e35e99356f9fe02f8e1a0912a2c
Anna Shishkova <asshishkova@gmail.com>
2022-03-01 12:11:50 +0200

    index and show Place

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :places
   root to: 'pages#home'
   resources :talents, only: %i[index show]
+  resources :places, only: %i[index show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 require "open-uri"
 
-puts 'Cleaning up PlaceDB'
+puts 'Cleaning up TalentDB'
 Talent.destroy_all
 
 talent_photos = ['https://www.warble-entertainment.com/blog/wp-content/uploads/2019/10/How-much-do-musicians-cost.jpg', 'https://musicindustryhowtoimages.s3.amazonaws.com/wp-content/uploads/2019/09/24183938/become-musician.jpg', 'https://s18670.pcdn.co/wp-content/uploads/Famous-Musicians-for-Kids.jpg', 'https://www.royalnavy.mod.uk/-/media/careers-section-redesign/homepage/roles-and-specialisations/services/marines/musician/roledetail_videoposter_647x364-assets/647x364_musician.jpg?h=364&w=647&rev=0124080e27394453b053e23bb219b652&cropregion=&hash=997DFA126796BE9EB2DB4FA04EC6A8AA', 'https://www.music.northwestern.edu/sites/default/files/2018-07/hire_musician.jpg', 'https://smallbiztrends.com/ezoimgfmt/media.smallbiztrends.com/2017/03/shutterstock_402500896-850x476.jpg?ezimgfmt=ng%3Awebp%2Fngcb12%2Frs%3Adevice%2Frscb12-2', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/A_musician_playing_violin_4.jpg/1280px-A_musician_playing_violin_4.jpg']
@@ -37,8 +37,8 @@ place_photos = ['https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/lon
     password: '123456',
     address: ["Afula", "Akko", "Arad", "Ariel", "Ashdod", "Ashkelon", "Baqa al-Gharbiyye", "Bat Yam", "Beer Sheva", "Beit Shean", "Beit Shemesh", "Betar Illit", "Bnei Berak", "Dimona", "Eilat", "Elad", "Givatayim", "Hadera", "Haifa", "Harish", "Herzliya", "Hod HaSharon", "Holon", "Jerusalem", "Karmiel", "Kfar Sava", "Kiryat Ata", "Kiryat Bialik", "Kiryat Gat", "Kiryat Malachi", "Kiryat Motzkin", "Kiryat Ono", "Kiryat Shemone", "Kiryat Yam", "Lod", "Maale Adumim", "Maalot Tarshiha", "Migdal HaEmek", "Modiin", "Nahariya", "Nazareth", "Nes Ziona", "Nesher", "Netanya", "Netivot", "Nof Hagalil", "Ofakim", "Or Akiva", "Or Yehuda", "Petah Tikva", "Qalansawe", "Raanana", "Rahat", "Ramat Hasharon", "Ramat-Gan", "Ramla", "Rehovot", "Rishon Lezion", "Rosh Ha'ayin", "Sakhnin", "Sderot", "Shefaram", "Taibeh", "Tamra", "Tel Aviv", "Tiberias", "Tira", "Tirat Carmel", "Tsfat (Safed)", "Umm al-Fahm", "Yavne", "Yehud-Monosson", "Yokneam"].sample,
   )
-  new_place.website = "#{new_place.title}-#{new_place.address}.co.il"
-  new_place.description = "Hi, we are cute restaurant '#{new_place.title}', located in the middle of #{new_place.address}. Looking for a talent to make our guests happy!"
+  new_place.website = "https://www.tripadvisor.com/Restaurants-g293984-Tel_Aviv_Tel_Aviv_District.html"
+  new_place.description = "Hi, we are a cute restaurant '#{new_place.title}', located in the middle of #{new_place.address}. Looking for a talent to make our guests happy!"
   file = URI.open(place_photos.sample)
   new_place.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
   new_place.save!
