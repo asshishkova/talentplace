e3bc62afc20b9dad1e16829940563ffe47a66c1d
Anna Shishkova <asshishkova@gmail.com>
2022-03-02 17:34:51 +0200

    authentification with the question who you are

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :talents
   devise_for :places
   root to: 'pages#home'
+  get 'login', to: 'pages#login', as: 'login'
   resources :talents, only: %i[index show edit update]
   resources :places, only: %i[index show]
 
   namespace :talent do
     get "dashboard", to: "pages#dashboard"
   end
+
 
   resources :bookings, only: :index # this route is for checking only
 
