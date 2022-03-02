b92598285225636f29aca8eb9413d429dd3663e1
Anna Shishkova <asshishkova@gmail.com>
2022-03-02 16:36:44 +0200

    now we can create new bookings being logged in as a place

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   devise_for :talents
   devise_for :places
   root to: 'pages#home'
-  resources :talents, only: %i[index show edit update]
+  resources :talents, only: %i[index show edit update] do
+    resources :bookings, only: %i[new create]
+  end
   resources :places, only: %i[index show]
 
   namespace :talent do
     get "dashboard", to: "pages#dashboard"
   end
 
-  resources :bookings, only: :index # this route is for checking only
+  # resources :bookings, only: :index # this route is for checking only
+  resources :bookings, except: %i[new create]
 
 end
