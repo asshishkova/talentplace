17e8185ca43265a83dd573d617c88c8875c19c06
Yoan Attias <yoanattias25@gmail.com>
2022-03-03 14:06:03 +0200

    dashboard

--- app/controllers/talent/pages_controller.rb
+++ app/controllers/talent/pages_controller.rb
@@ -2,4 +2,11 @@ class Talent::PagesController < ApplicationController
   def dashboard
 
   end
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
 end
