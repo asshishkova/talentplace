6146c8749dd4448880781e18284bc0d3a04641c0
Yoan Attias <yoanattias25@gmail.com>
2022-03-03 13:49:50 +0200

    finished the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
     get "dashboard", to: "pages#dashboard"
   end
 
+  namespace :place do
+    get "dashboard", to: "pages#dashboard"
+  end
+
   resources :bookings, only: :index # this route is for checking only
 
 end
