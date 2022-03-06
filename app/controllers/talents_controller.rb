765a786b73d29422c5ec01f2f32a845ec1893fd7
Anna Shishkova <asshishkova@gmail.com>
2022-03-06 15:14:58 +0200

    genres edit for talents

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -9,9 +9,19 @@ class TalentsController < ApplicationController
   end
 
   def edit
+    talent_genres = Genre.where.not(id: [@talent.genres.pluck(:id)]).map do |genre|
+      TalentGenre.new(genre: genre, talent: current_talent)
+    end
+    @all_genres = talent_genres + @talent.talent_genres
+    # @talent_genres = @talent.genres
   end
 
   def update
+    @talent.talent_genres.destroy_all
+    genre_ids = params[:talent][:genre_ids].split(',').map(&:to_i)
+    genre_ids.each do |genre_id|
+      TalentGenre.create!(talent: current_talent, genre: Genre.find_by(id: genre_id))
+    end
     @talent.update(talent_params)
     redirect_to talent_path(@talent)
   end
@@ -20,7 +30,7 @@ end
 private
 
 def talent_params
-  params.require(:talent).permit(:name, :description, :address, :youtube_link, :instagram_link, :spotify_link, :price, :photo)
+  params.require(:talent).permit(:name, :description, :address, :youtube_link, :instagram_link, :spotify_link, :price, :photo, :address)
 end
 
 def set_talent
